25195d62096e53e543a463ff5af6140606d73883
Jostein Zheng <jostein.zheng@gmail.com>
2017-09-12 11:33:45 +0800

    update bash config

--- zhengzhijie.bash
+++ zhengzhijie.bash
@@ -145,6 +145,9 @@ export SDKMAN_DIR="/Users/jostein/.sdkman"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 export NVM_DIR="$HOME/.nvm"
-. "$(brew --prefix nvm)/nvm.sh"
+if test "Linux" != $(uname);then
+  . "$(brew --prefix nvm)/nvm.sh"
+fi
+
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
